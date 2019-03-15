5a70cca0ddeb807934b4ca6132c24f13b73193b8
Antony.j.m <antonyjosemalakkaran@gmail.com>
2019-03-15 11:59:56 +0530

    4

--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            if user:
+                messages.success(request,f'Accounts Created for {username}! You can now log in')
+                login(request, user)
+            return redirect('login')  
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+
