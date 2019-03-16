ce52cd49b4eb656a534d5ce22f8b24eecd9edfa9
arjungnair <arjungnair2013@gmail.com>
2019-03-16 11:45:50 +0530

    name of company

---
+++ 1st/name.py
@@ -0,0 +1,13 @@
+import pandas as pd
+ 
+df = pd.read_csv('secwiki_tickers.csv')
+dp = pd.read_csv('file.lst',names=['pTicker'])
+ 
+pTickers = dp.pTicker.values  # converts into a list
+ 
+tmpTickers = []
+ 
+for i in range(len(pTickers)):
+    test = df[df.Ticker==pTickers[i]]
+    if not (test.empty):
+        print("%-10s%s" % (pTickers[i], list(test.Name.values)[0]))
