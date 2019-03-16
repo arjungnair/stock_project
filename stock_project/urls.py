65edb40a28365135b6eb9b56048e4b71bc81ea49
Antony.j.m <antonyjosemalakkaran@gmail.com>
2019-03-16 09:43:59 +0530

    commit5

--- stock_project/urls.py
+++ stock_project/urls.py
@@ -5,5 +5,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('/fav', include('fav.urls')),
+    path('fav', include('fav.urls')),
 ]
